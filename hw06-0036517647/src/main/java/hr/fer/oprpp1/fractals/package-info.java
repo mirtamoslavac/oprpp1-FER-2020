deb405c919604aef7de6286f05e7a6302deb5b27
Mirta Moslavac <mirta.moslavac@fer.hr>
2020-12-14 01:00:03 +0100

    add respective package-info.java files

---
+++ hw06-0036517647/src/main/java/hr/fer/oprpp1/fractals/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing implementations of both single- and multi-threaded Newton-Raphson iteration-based fractal viewers.
+ *
+ * @since 6.0
+ */
+package hr.fer.oprpp1.fractals;
---
+++ hw06-0036517647/src/main/java/hr/fer/oprpp1/math/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing implementations of a complex number and respective polynomials.
+ *
+ * @since 6.0
+ */
+package hr.fer.oprpp1.math;
