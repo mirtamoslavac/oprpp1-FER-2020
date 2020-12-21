66b260190455cfa0302394422c0edbae2e3fe7bf
Mirta Moslavac <mirta.moslavac@fer.hr>
2020-12-21 21:03:36 +0100

    implement primes application

---
+++ hw07-0036517647/src/main/java/hr/fer/oprpp1/gui/prim/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing an implementation of a simple swing application for displaying two equal lists of dynamically generated primes.
+ *
+ * @since 7.0
+ */
+package hr.fer.oprpp1.gui.prim;
